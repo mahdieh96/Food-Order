330afdec8a0196d9fd5326a2f0363bc63c9fb064
mahdie <jafari.mahdieh@gmail.com>
2023-08-19 18:44:56 +0330

    general main content added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Header } from "./Components/Layout/Header";
+import { Meals } from "./Components/Meals/Meals";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
