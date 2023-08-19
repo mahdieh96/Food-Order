24a0f91891e404bddf782d063eb7b43f2552ff4c
mahdie <jafari.mahdieh@gmail.com>
2023-08-19 18:10:15 +0330

    ShoppingCartButton added.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import { HeaderCartButton } from "./HeaderCartButton";
 export const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>click</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="a table full of delicious food!" />
